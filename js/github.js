6b9869ce6e91096ea3ecd43fbd3594d9a9438895
Angie Smith <avksmit2@gmail.com>
2016-10-14 13:17:05 -0700

    add logic to allow a user without a key to run site.

--- js/github.js
+++ js/github.js
@@ -5,15 +5,19 @@ function Search() {
 }
 
 Search.prototype.getUser = function(user, displayUserInfo, displayRepositories) {
-  $.get('https://api.github.com/search/repositories?q=user:'+ user + '&sort=updated&order:desc&per_page=100&access_token=' + apiKey).then(function(response){
-    console.log(response.items[0].owner.avatar_url);
+  if (apiKey !== "") {
+    var query = '&sort=updated&order:desc&per_page=100&access_token=' + apiKey;
+  } else {
+    var query = '&sort=updated&order:desc&per_page=100';
+  }
+  
+  $.get('https://api.github.com/search/repositories?q=user:'+ user + query).then(function(response){
 
     response.items.forEach(function(repository) {
       var dateCreated = moment(repository.created_at).format('MM/DD/YYYY');
 
       displayRepositories(repository.name, repository.language, repository.description, dateCreated);
     });
-
     displayUserInfo(user, response.items.length, response.items[0].owner.avatar_url);
     if (response.items.length === 0) {
       displayNoResults("This user has no repositories!");
