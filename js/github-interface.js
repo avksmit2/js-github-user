7755f43972ce2b90789bcb75f99d5476d45b3a72
Angie Smith <avksmit2@gmail.com>
2016-10-14 11:15:25 -0700

    add the date created field and sort it descending.

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,8 +1,8 @@
 var Search = require('./../js/github.js').searchModule;
 
-var displayRepositories = function(user, repositoryName, repositoryLanguage, repositoryDescription) {
+var displayRepositories = function(user, repositoryName, repositoryLanguage, repositoryDescription, repositoryCreated) {
   $('.user-name').text('User: ' + user);
-  $('#results').append('<tr><td>' + repositoryName + '</td> <td>' + repositoryLanguage + '</td> <td>' + repositoryDescription + '</td></tr>');
+  $('#results').append('<tr><td>' + repositoryName + '</td> <td>' + repositoryLanguage + '</td> <td>' + repositoryDescription + '</td> <td>' + repositoryCreated + '</td></tr>');
 };
 
 $(document).ready(function() {
--- js/github.js
+++ js/github.js
@@ -5,10 +5,10 @@ function Search() {
 }
 
 Search.prototype.getUser = function(user, displayFunction) {
-  $.get('https://api.github.com/search/repositories?q=user:'+ user + '&access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/search/repositories?q=user:'+ user + '&sort=updated&order:desc&access_token=' + apiKey).then(function(response){
     response.items.forEach(function(repository) {
-      // console.log(repository.description);
-      displayFunction(user, repository.name, repository.language, repository.description);
+      var dateCreated = moment(repository.created_at).format('MM/DD/YYYY');
+      displayFunction(user, repository.name, repository.language, repository.description, dateCreated);
     });
   }).fail(function(error){
     console.log(error.responseJSON.message);
