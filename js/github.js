e815ad70d31975614918b7e84fd99efc8aa539c3
Angie Smith <avksmit2@gmail.com>
2016-10-14 11:19:20 -0700

    change get function so that the number of items returned is set to the cap of 100.

--- js/github.js
+++ js/github.js
@@ -5,7 +5,8 @@ function Search() {
 }
 
 Search.prototype.getUser = function(user, displayFunction) {
-  $.get('https://api.github.com/search/repositories?q=user:'+ user + '&sort=updated&order:desc&access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/search/repositories?q=user:'+ user + '&sort=updated&order:desc&per_page=100&access_token=' + apiKey).then(function(response){
+    console.log(response.length);
     response.items.forEach(function(repository) {
       var dateCreated = moment(repository.created_at).format('MM/DD/YYYY');
       displayFunction(user, repository.name, repository.language, repository.description, dateCreated);
